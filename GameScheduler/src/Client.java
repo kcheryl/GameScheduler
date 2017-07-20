cf27905cb92b6918d81968fd33262e9424c1a02c
Cheryl Kee <ksy.cheryl@gmail.com>
2017-07-20 09:06:52 +0800

    Remove comments from Client

--- GameScheduler/src/Client.java
+++ GameScheduler/src/Client.java
@@ -13,44 +13,14 @@ public class Client {
 	}
 
 	public static String addGame(String name, int numPlayers) {
-		// String response;
-		// if (name != null && name.matches(".*[a-z].*") && numPlayers > 0) {
-		// response = service.createGame(new Game(name, numPlayers));
-		// } else if (numPlayers < 1) {
-		// response = "Invalid number of players!";
-		// } else {
-		// response = "Invalid game name!";
-		// }
-		// System.out.println(response);
-		// return response;
 		return service.createGame(new Game(name, numPlayers));
 	}
 
 	public static String addPlayer(String name, Game[] games) {
-		// String response;
-		// if (name != null && name.matches(".*[a-z].*") && games.length > 0) {
-		// response = service.createPlayer(new Player(name, games));
-		// } else if (games.length < 1) {
-		// response = "Invalid number of games!";
-		// } else {
-		// response = "Invalid player name!";
-		// }
-		// System.out.println(response);
-		// return response;
 		return service.createPlayer(new Player(name, games));
 	}
 
 	public static String addDay(String name, Game[] games) {
-		// String response;
-		// if (name != null && name.matches(".*[a-z].*") && games.length > 0) {
-		// response = service.createDay(new Day(name, games));
-		// } else if (games.length < 1) {
-		// response = "Invalid number of games!";
-		// } else {
-		// response = "Invalid day name!";
-		// }
-		// System.out.println(response);
-		// return response;
 		return service.createDay(new Day(name, games));
 	}
 
